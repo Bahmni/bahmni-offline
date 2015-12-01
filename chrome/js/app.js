d6628d17735cb824b4512c23087f715e9cd003a1
padma <padmavb@thoughtworks.com>
2015-12-01 17:03:51 +0530

    Padma, Ranganathan | Implemtor able to enter the IP of the machine which he/she wants to test/use

--- chrome/js/app.js
+++ chrome/js/app.js
@@ -1,4 +1,40 @@
-function maximizeWebviews() {
+document.addEventListener('DOMContentLoaded', function () {
+    getFromDB('implementationURL', handleImplementationURL);
+}, false);
+
+var handleImplementationURL = function(data) {
+
+    if (data && data.implementationURL) {
+        hideImplementationInputs();
+        document.querySelector('webview').setAttribute(
+            'src', data.implementationURL);
+    }
+    else {
+        enterButton.addEventListener('click', function () {
+            hideImplementationInputs();
+            var source = "https://" +  document.getElementById('ipValue').value + "/bahmni/home/#/device/chrome-app";
+            document.querySelector('webview').setAttribute('src', source);
+            setInDB({'implementationURL': source});
+        });
+    }
+};
+
+var setInDB= function(object){
+    chrome.storage.local.set(object);
+};
+
+var getFromDB= function(key, promise){
+    chrome.storage.local.get(key, function(data){
+        promise(data);
+    });
+};
+
+var hideImplementationInputs = function() {
+    document.getElementById('enterButton').style.display = 'none';
+    document.getElementById('ipValue').style.display = 'none';
+};
+
+var maximizeWebviews = function () {
     var webview = document.querySelector("webview");
     webview.style.height = document.documentElement.clientHeight + "px";
     webview.style.width = document.documentElement.clientWidth + "px";
