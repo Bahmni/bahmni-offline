efe58ac1ba390c51fc4cc9abc8c1f11fe879d483
abisheka <abishek91@gmail.com>
2016-01-18 18:08:14 +0530

    Abishek/Vikash | Making roboelectric as a compile dependency instead of test dependency because it doesnt work as a test dependency

--- android/platforms/android/src/test/java/org/bahmni/offline/OfflineDaoTest.java
+++ android/platforms/android/src/test/java/org/bahmni/offline/OfflineDaoTest.java
@@ -2,13 +2,15 @@ package org.bahmni.offline;
 
 import android.test.AndroidTestCase;
 import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.robolectric.RobolectricGradleTestRunner;
-//import org.robolectric.annotation.Config;
-//
-//@RunWith(RobolectricGradleTestRunner.class)
-//@Config(constants = BuildConfig.class, sdk=18)
-public class OfflineDaoTest{
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk=18)
+
+public class OfflineDaoTest extends AndroidTestCase{
 
     @Test
     public void shouldPrint() throws Exception {
