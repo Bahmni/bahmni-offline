82746badaa917b545aaf0b22103b0943982d95ec
abisheka <abishek91@gmail.com>
2016-02-11 10:31:13 +0530

    Abishek | #914 | Allowing cookies for files served locally

--- android/platforms/android/src/main/java/org/bahmni/offline/MainActivity.java
+++ android/platforms/android/src/main/java/org/bahmni/offline/MainActivity.java
@@ -1,9 +1,9 @@
 package org.bahmni.offline;
 
 import android.os.Bundle;
-
 import org.apache.cordova.CordovaActivity;
 import org.bahmni.offline.services.DbService;
+import org.xwalk.core.XWalkCookieManager;
 import org.xwalk.core.XWalkPreferences;
 import org.xwalk.core.XWalkView;
 
@@ -15,6 +15,9 @@ public class MainActivity extends CordovaActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         XWalkView xWalkWebView = (XWalkView) findViewById(R.id.xwalkWebView);
+        XWalkCookieManager mCookieManager = new XWalkCookieManager();
+        mCookieManager.setAcceptCookie(true);
+        mCookieManager.setAcceptFileSchemeCookies(true);
         xWalkWebView.addJavascriptInterface(new DbService(MainActivity.this), "AndroidOfflineService");
         xWalkWebView.loadAppFromManifest("file:///android_asset/manifest.json", null);
         // turn on debugging
