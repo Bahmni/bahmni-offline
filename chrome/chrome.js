5e177e91842d7fbd5b36405f58d54b8b614c3e35
Ranganathan Balashanmugam <ranganab@thoughtworks.com>
2016-02-16 16:03:16 +0530

    Ranganathan | #916 | Added home page to collect implementation url for chrome

---
+++ chrome/chrome.js
@@ -0,0 +1,28 @@
+window.onload = function(e){
+    handleImplementationURL(localStorage.getItem('host'));
+}
+
+var handleImplementationURL = function(data) {
+  if (data) {
+    hideImplementationInputs();
+    goToHome();
+  } else {
+    enterButton.addEventListener('click', function() {
+      var source = document.getElementById('ipValue').value;
+      if(source){
+        hideImplementationInputs();
+        localStorage.setItem('host',source);
+        goToHome();
+      }
+    });
+  }
+};
+
+var hideImplementationInputs = function() {
+  document.getElementById('enterButton').style.display = 'none';
+  document.getElementById('ipValue').style.display = 'none';
+};
+
+var goToHome = function(){
+  window.location = "app/home/index.html#/login";
+};
