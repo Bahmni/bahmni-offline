4132e317fded5c1cabc8e90a3db1e303246d0b04
shridhar kalagi <shridharkalagi@gmail.com>
2020-12-10 18:24:22 +0530

    add CI

--- scripts/getArtefact.sh
+++ scripts/getArtefact.sh
@@ -4,12 +4,10 @@ export LANG=en_US.UTF-8
 set -e
 
 echo "in script file"
-
 cd ..
-
-curl -o apps.zip  --location --request GET 'https://api.github.com/repos/Bahmni/bahmni-connect/actions/artifacts/29404996/zip' \
+curl -o android.zip  --location --request GET 'https://api.github.com/repos/Bahmni/bahmni-connect/actions/artifacts/29404996/zip' \
 --header "$GIT_AUTH_HEADER"
-
 ls
-
+unzip -o android.zip
+ls
 echo "Downloaded the artefact"
