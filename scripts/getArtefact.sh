e549598bc244ef25be633a53e171ed7ab7cbefbe
shridhar kalagi <shridharkalagi@gmail.com>
2020-12-10 17:55:10 +0530

    add CI

--- scripts/getArtefact.sh
+++ scripts/getArtefact.sh
@@ -4,4 +4,4 @@ export LANG=en_US.UTF-8
 set -e
 
 echo "in script file"
-curl -o apps.zip --location -H "Authorization: token " https://api.github.com/repos/Bahmni/bahmni-connect/actions/artifacts/29404996/zip
+curl -o apps.zip --location -H $GIT_AUTH_HEADER https://api.github.com/repos/Bahmni/bahmni-connect/actions/artifacts/29404996/zip
