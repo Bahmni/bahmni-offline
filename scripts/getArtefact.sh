742db4363d048bf8332721ffe06d17043c5bb158
shridhar kalagi <shridharkalagi@gmail.com>
2020-12-10 18:14:11 +0530

    add CI

--- scripts/getArtefact.sh
+++ scripts/getArtefact.sh
@@ -5,4 +5,4 @@ set -e
 
 echo "in script file"
 curl -o apps.zip  --location --request GET 'https://api.github.com/repos/Bahmni/bahmni-connect/actions/artifacts/29404996/zip' \
---header $GIT_AUTH_HEADER
+--header "$GIT_AUTH_HEADER"
