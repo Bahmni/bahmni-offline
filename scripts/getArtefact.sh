096d340a1fea09904359b3e0cc887a32a554774b
Angshuman Sarkar <angshuonline@gmail.com>
2021-04-12 16:11:25 +0530

    Updating bahmni-connect android artefact version

--- scripts/getArtefact.sh
+++ scripts/getArtefact.sh
@@ -5,7 +5,7 @@ set -e
 
 echo "in script file"
 cd ..
-curl -o android.zip  --location --request GET 'https://api.github.com/repos/Bahmni/bahmni-connect/actions/artifacts/44155168/zip' \
+curl -o android.zip  --location --request GET 'https://api.github.com/repos/Bahmni/bahmni-connect/actions/artifacts/53217087/zip' \
 -H "Authorization: token $SECRET_AUTH_TOKEN"
 ls
 unzip -o android.zip
