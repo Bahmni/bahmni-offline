ffd3bb4d2decacafa230c4d5a321800ffd1ac3ef
Raushan <rkraushan113@gmail.com>
2023-02-06 12:39:56 +0530

    modified:   scripts/getArtefact.sh

--- scripts/getArtefact.sh
+++ scripts/getArtefact.sh
@@ -5,7 +5,7 @@ set -e
 
 echo "in script file"
 cd ..
-curl -o android.zip  --location --request GET 'https://api.github.com/Rkum113/bahmni-connect/actions/runs/4022218784/zip' \
+curl -o android.zip  --location --request GET 'https://api.github.com/repos/Rkum113/bahmni-connect/actions/artifacts/538249103/zip' \
 -H "Authorization: token $SECRET_AUTH_TOKEN"
 ls
 unzip -o android.zip -d androidDist
